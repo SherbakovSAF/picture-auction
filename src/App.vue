0b32c9b1574fa3e9ec2ff769df6a8d3f3e68bf40
SherbakovSAF <sherbakovsaf@gmail.com>
2023-05-13 17:20:31 +0300

    Фикс вёрстки

--- src/App.vue
+++ src/App.vue
@@ -40,6 +40,7 @@
           <h3 class="main__item__content__sales-title">Продано на аукционе</h3>
         </div>
       </article>
+      
       <article>
         <div class="main__item__content__wrap">
           <img src="./assets/media/img/cardImage/venera.jpg" alt="">
@@ -82,20 +83,7 @@
           <h3 class="main__item__content__sales-title">Продано на аукционе</h3>
         </div>
       </article>
-      <article>
-        <div class="main__item__content__wrap">
-          <img src="./assets/media/img/cardImage/venera.jpg" alt="">
-          <h2>«Рождение Венеры» Сандро Боттичелли</h2>
-          <div class="main__item__content__interaction">
-            <div class="main__item__content__price__wrap">
-              <h6 class="main__item__content__price-sale">1 000 000 $</h6>
-              <h3>1 000 000 $</h3>
-            </div>
-            <button class="btn">Купить</button>
-          </div>
-          <h3 class="main__item__content__sales-title">Продано на аукционе</h3>
-        </div>
-      </article>
+      
     </div>
   </main>
 <footer>
@@ -114,8 +102,7 @@
     </div>
     <div class="footer__block__contact">
       <a href="tel:8495999999999"><h5 class="footer__block__contact__number">+7 (495) 999-99-99</h5></a>
-      <a href="tel:8495999999999"><h5 class="footer__block__contact__address">г. Воронеж, ул. Никитина, 119А</h5></a>
-  
+      <a target="_blank" href="https://yandex.ru/maps/193/voronezh/house/ulitsa_nikitina_119/Z0AYcw5kTUwCQFtrfXR2eHRqZw==/?ll=39.594495%2C51.879893&utm_source=share&z=17"><h5 class="footer__block__contact__address">г. Воронеж, ул. Никитина, 119А</h5></a>
     </div>
   </div>
 </footer>
