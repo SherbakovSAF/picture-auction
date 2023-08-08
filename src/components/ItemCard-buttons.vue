9221c571ffc5bfc550f6605b2b814c28184d4d1f
SherbakovSAF <sherbakovsaf@gmail.com>
2023-08-08 20:04:29 +0300

    Вынесение логики кнопко карточки в отдельный компонент

---
+++ src/components/ItemCard-buttons.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="btn" :class="{'btn-checked': isSelected}">
+    <preloader-buttons v-if="isLoader"/>
+    <div v-else class="main__item__content__price__is-selected">
+      <img v-show="isSelected" src="../../public/img/buyCheck.svg" alt="Купленно">
+      <h4>{{ isSelected ? 'В корзине' : 'Купить'}}</h4>
+    </div>
+  </button>
+</template>
+
+
+<script>
+import PreloaderButtons from './Preloader-Buttons.vue';
+export default {
+  name: 'ItemCard',
+  components: {
+    PreloaderButtons
+  },
+  props: {
+    isSelected: {
+      type: Boolean,
+      required: true
+    },
+    isLoader: {
+      type: Boolean,
+      required: false
+    }
+  },
+}
+</script>
