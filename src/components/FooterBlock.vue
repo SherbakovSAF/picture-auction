790f5a6d5c03db6a426ef40bf9050401d1d11c39
SherbakovSAF <sherbakovsaf@gmail.com>
2023-08-19 17:09:47 +0300

    Адаптация подвальной панели

--- src/components/FooterBlock.vue
+++ src/components/FooterBlock.vue
@@ -1,13 +1,13 @@
 <template>
-     <footer>
-     <div class="footer__block container">
-      <nav-module />
-       <div class="footer__block__contact">
-         <a href="tel:8495999999999"><h5 class="footer__block__contact__number">+7 (495) 999-99-99</h5></a>
-         <a target="_blank" href="https://yandex.ru/maps/193/voronezh/house/ulitsa_nikitina_119/Z0AYcw5kTUwCQFtrfXR2eHRqZw==/?ll=39.594495%2C51.879893&utm_source=share&z=17"><h5 class="footer__block__contact__address">г. Воронеж, ул. Никитина, 119А</h5></a>
-       </div>
-     </div>
-   </footer>
+  <footer>
+    <div class="footer__block container">
+    <nav-module class="footer__block__nav"/>
+      <div class="footer__block__contact">
+        <a href="tel:8495999999999"><h5 class="footer__block__contact__number">+7 (495) 999-99-99</h5></a>
+        <a target="_blank" href="https://yandex.ru/maps/193/voronezh/house/ulitsa_nikitina_119/Z0AYcw5kTUwCQFtrfXR2eHRqZw==/?ll=39.594495%2C51.879893&utm_source=share&z=17"><h5 class="footer__block__contact__address">г. Воронеж, ул. Никитина, 119А</h5></a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
